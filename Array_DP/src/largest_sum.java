4add76af18643b436797112abc7dd8b35f05cf7f
Niilo Vuokila <59707802+niilovuo@users.noreply.github.com>
2021-05-02 16:28:25 -0400

    Update on Array_DP problem solution

--- Array_DP/src/largest_sum.java
+++ Array_DP/src/largest_sum.java
@@ -9,6 +9,10 @@ public class largest_sum {
      *
      * Given a list of integers, write a function that returns the largest sum of non-adjacent numbers.
      * Numbers can be negative or positive integers, including 0.
+     * For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5.
+     * [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+     *
+     * Source of problem: https://www.dailycodingproblem.com/ (wording of problem has been modified)
      *
      * @param arr   The array to be used for the problem
      * @return      The greatest non-adjacent sum from the input array. Returns 0 if all negative numbers.
@@ -17,17 +21,13 @@ public class largest_sum {
         int[] dp_array = new int[arr.length];
         // Set the first required values for the dynamic programming table.
         dp_array[0] = Math.max(0, arr[0]);
-        dp_array[1] = Math.max(arr[0], arr[1]);
+        dp_array[1] = Math.max(dp_array[0], arr[1]);
         // Calculate the largest possible value for every position in the input array in a bottom-up manner.
         for(int i = 2; i < arr.length; i++){
             dp_array[i] = Math.max(dp_array[i-1], arr[i] + dp_array[i-2]);
         }
-        // Go over the array once more, to find the maximum computed value.
-        int max = Integer.MIN_VALUE;
-        for(int val : dp_array){
-            if(max < val) max = val;
-        }
-        return max;
+        // Return the largest value, which is at the end of dp_array.
+        return dp_array[dp_array.length - 1];
     }
 
     private static void printArray(int[] arr){
@@ -48,7 +48,7 @@ public class largest_sum {
             // Assigns a value from the range [-10, 10] to each index.
             arr[i] = r.nextInt(20) - 10;
         }
-        System.out.println("The array is:");
+        System.out.print("The array is: ");
         printArray(arr);
         System.out.println("The largest sum found is: " + findLargestSum(arr));
     }
